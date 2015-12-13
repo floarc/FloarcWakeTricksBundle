8353f91514e4232e1e0de6e9511288a75ed195b7
floarc <florent.arcambal@gmail.com>
2015-12-13 10:21:28 +0100

    First Commit

---
+++ FloarcWakeTricksBundle.php
@@ -0,0 +1,11 @@
+<?php
+// File: FloarcWakeTricksBundle.php
+
+namespace Floarc\FloarcWakeTricksBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FloarcWakeTricksBundle extends Bundle
+{
+}
