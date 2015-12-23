71ca3c5dc7376ff5afb8043119378402cfa9da8d
floarc <florent.arcambal@gmail.com>
2015-12-23 15:14:25 +0100

    Adding some conf, doctrine bundle and first entity "Hand"

---
+++ Entity/Hand.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace FloarcWakeTricksBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Hand
+ *
+ * @ORM\Table(name="hand")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\HandRepository")
+ */
+class Hand
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=10, unique=true)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=20, unique=true)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return Hand
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Hand
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
--- Tests/app/AppKernel.php
+++ Tests/app/AppKernel.php
@@ -11,11 +11,12 @@ class AppKernel extends Kernel
         return array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Floarc\WakeTricksBundle\FloarcWakeTricksBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
         );
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
